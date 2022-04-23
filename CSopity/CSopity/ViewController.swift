0dedfe619e4ec6d858efaed98f3b003701aab142
MAIMINEM <104252557+MAIMINEM@users.noreply.github.com>
2022-04-23 17:59:36 +0800

    create project

---
+++ CSopity/CSopity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSopity
+//
+//  Created by AIMINEM on 23/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
